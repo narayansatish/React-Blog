2aec2358715be45e8cab3f50c8a8755d57e78f23
Satish Narayan <narayansatish97@gmail.com>
2021-01-14 17:14:02 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
+import React from 'react'
+
+
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button,Image     ,Row,Col  ,Jumbotron } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './component/Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NavBar/>
     </div>
   );
 }
