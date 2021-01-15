4799ff5db2eb1433a421ee20805ff764a065cf87
Satish Narayan <narayansatish97@gmail.com>
2021-01-15 10:03:38 +0530

    Day 1

--- src/component/NewBlog.js
+++ src/component/NewBlog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState} from 'react';
 import {
         BrowserRouter as Router,
         Switch,
@@ -18,30 +18,124 @@ import {
         Col,
 } from "react-bootstrap";
 function NewBlog() {
+        let [title,setTitle]=useState("");
+        let [author,setAuthor]=useState("");
+        let [category,setCategory]=useState("");
+        let [content,setContent]=useState("");
+        const publishNow=()=>{
+                console.log(title);
+                console.log(author);
+                console.log(category);
+                console.log(content);
+                let post={
+                        "title":title,
+                        "author":author,
+                        "category":category,
+                        "content":content,
+                        "date": (new Date().toLocaleDateString())
+
+                };
+                post=JSON.stringify(post);
+                console.log(post);
+
+                setTitle("");
+                setAuthor("");
+                setCategory("");
+                setContent("");
+
+        };
         let element = (
-                <Row className="mb-5 fixed-bottom d-flex">
-                        <Col md={{ span: 1, offset: 11 }}>
-                                <Link to="/">
-                                        <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="64"
-                                                height="64"
-                                                fill="red"
-                                                class="bi bi-house-fill"
-                                                viewBox="0 0 16 16"
-                                        >
-                                                <path
-                                                        fill-rule="evenodd"
-                                                        d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
-                                                />
-                                                <path
-                                                        fill-rule="evenodd"
-                                                        d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
-                                                />
-                                        </svg>
-                                </Link>
-                        </Col>
-                </Row>
+                <>
+                        <Row className="mt-5">
+                                <Col sm={{ span: 4, offset: 4 }}>
+                                        <span >
+                                        <Form>
+                                                <Form.Group controlId="exampleForm.ControlInput1">
+                                                        <Form.Label>
+                                                                Title
+                                                        </Form.Label>
+                                                        <Form.Control
+                                                                type="text"
+                                                                placeholder="Title"
+                                                                value={title}
+                                                                onChange={(e)=>{setTitle(e.target.value)}}
+                                                        />
+                                                </Form.Group>
+                                                <Form.Group controlId="exampleForm.ControlSelect1">
+                                                        <Row>
+                                                                <Col>
+                                                                        <Form.Group controlId="exampleForm.ControlInput1">
+                                                                                <Form.Label>
+                                                                                        
+                                                                                        Author
+                                                                                </Form.Label>
+                                                                                <Form.Control
+                                                                                        type="text"
+                                                                                        placeholder="Post Author"
+                                                                                        value={author}
+                                                                                        onChange={(e)=>{setAuthor(e.target.value)}}
+                                                                                />
+                                                                        </Form.Group>
+                                                                </Col>
+                                                                <Col>
+                                                                        <Form.Label>
+                                                                                Category
+                                                                        </Form.Label>
+                                                                        <Form.Control as="select" value={category} onChange={(e)=>{setCategory(e.target.value)}}>
+                                                                                <option>
+                                                                                        Redux
+                                                                                </option>
+                                                                                <option>
+                                                                                        React
+                                                                                </option>
+                                                                                <option>
+                                                                                        Udacity
+                                                                                </option>
+                                                                        </Form.Control>
+                                                                </Col>
+                                                        </Row>
+                                                </Form.Group>
+
+                                                <Form.Group controlId="exampleForm.ControlTextarea1">
+                                                        <Form.Label>
+                                                                Post Content
+                                                        </Form.Label>
+                                                        <Form.Control
+                                                                as="textarea"
+                                                                rows={3}
+                                                                value={content}
+                                                                onChange={(e)=>{setContent(e.target.value)}}
+                                                        />
+                                                </Form.Group>
+                                                <Button variant="primary" onClick={publishNow}>Publish Now</Button>
+                                        </Form>
+                                        </span>
+                                </Col>
+                        </Row>
+                        <Row className="mb-5 fixed-bottom d-flex">
+                                <Col md={{ span: 1, offset: 11 }}>
+                                        <Link to="/">
+                                                <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="64"
+                                                        height="64"
+                                                        fill="red"
+                                                        class="bi bi-house-fill"
+                                                        viewBox="0 0 16 16"
+                                                >
+                                                        <path
+                                                                fill-rule="evenodd"
+                                                                d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
+                                                        />
+                                                        <path
+                                                                fill-rule="evenodd"
+                                                                d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
+                                                        />
+                                                </svg>
+                                        </Link>
+                                </Col>
+                        </Row>
+                </>
         );
         return element;
 }
